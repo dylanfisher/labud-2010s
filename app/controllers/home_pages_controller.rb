8464167a08e568dc1ffcb77203c9821b8ce9fc7c
dylanfisher <hi@dylanfisher.com>
2020-02-13 21:32:37 -0500

    Home page list view

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,6 +1,6 @@
 class HomePagesController < PagesController
   def show
-    @posts = Post.all
+    @posts = Post.includes(:media_item, :user).all.by_username
     @no_header = true
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,15 @@ class Post < Forest::ApplicationRecord
   belongs_to :media_item
 
   scope :by_user, -> (user) { where(user: user) }
+  scope :by_username, -> { joins(:user).order('users.last_name ASC, users.first_name ASC, users.email ASC') }
 
   validates_presence_of :title
 
   def self.resource_description
     'Each post represents a page of your project.'
+  end
+
+  def first_sentence
+    blocks.find { |b| b.try(:text).present? }.try(:text).to_s.truncate_words(40)
   end
 end
