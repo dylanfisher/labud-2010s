90e1f765dab6064c13666c15976eaa62c29c57ed
dylanfisher <hi@dylanfisher.com>
2020-02-14 00:20:09 -0500

    Better slideshow blocks

--- app/assets/javascripts/partials/slideshow_blocks.js
+++ app/assets/javascripts/partials/slideshow_blocks.js
@@ -5,7 +5,16 @@ $(document).on('click', '.slideshow-block', function() {
   var $allSlides = $block.find('.slideshow-block__slide');
   var $activeSlide = $allSlides.filter('.active').first();
   var $nextSlide = $activeSlide.nextWrap();
+  var maxZIndex = 0;
 
-  $allSlides.removeClass('active');
-  $nextSlide.addClass('active');
+  $allSlides.removeClass('active').each(function() {
+    var $thisSlide = $(this);
+    var thisZIndex = parseInt( $thisSlide.css('zIndex') ) || 0;
+
+    if ( thisZIndex > maxZIndex ) {
+      maxZIndex = thisZIndex;
+    }
+  });
+
+  $nextSlide.addClass('active').css({ zIndex: maxZIndex + 1 });
 });
